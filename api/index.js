cca474818272148d50597a14ccb42f5ae1a28607
Adarsh Jain <adarshjain1338@gmail.com>
2026-07-06 20:51:52 +0530

    disabled one api end point

--- api/index.js
+++ api/index.js
@@ -10,7 +10,7 @@ app.use("/auth",      require("../api/auth"));
 app.use("/config",    require("../api/config"));
 app.use("/settings",  require("../api/settings"));
 app.use("/send",      require("../api/send"));
-app.use("/summary",   require("../api/summary"));
+// app.use("/summary",   require("../api/summary"));
 app.use("/resume",    require("../api/resume"));
 app.use("/ai",        require("../api/ai"));
 app.use("/users",     require("../api/users"));
